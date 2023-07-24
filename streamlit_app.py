a268c40e6406ccf0ae05929dd61d0628e3e03d75
Sivasakthivelp <95265603+Sivasakthivelp@users.noreply.github.com>
2023-07-24 10:28:25 +0530

    Update streamlit_app.py
    Menu Adding

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 # created the main python file
 import streamlit
-streamlit.title("Sivasakthivel")
+treamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
