e32ca229c4d11fe555f0c36892da7c7c3f3d7e73
Sivasakthivelp <95265603+Sivasakthivelp@users.noreply.github.com>
2023-07-31 15:45:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 
-@streamlit.experimental_singleton
-def init_connection():
- return snowflake.connector.connect(
-**st.secrets[“snowflake”], client_session_keep_alive=True
-)
-conn = init_connection()
-
-
-
-
 
 streamlit.title("My parents New Healthy dinner")
 
@@ -45,12 +35,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
 
 
 
